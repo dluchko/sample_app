37a55cbe9c6da77e478430d288683c7c4ddabc56
Dmytro Luchko <dmytrluchko@gmail.com>
2016-02-14 15:12:17 +0100

    Circles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213190229) do
+ActiveRecord::Schema.define(version: 20160214132724) do
+
+  create_table "micriposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "micriposts", ["user_id", "created_at"], name: "index_micriposts_on_user_id_and_created_at"
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
     t.string   "name"
